d28157117cc496724a33645d050c9ca23904d984
tsukigatari <ndfz@proton.me>
2025-01-10 03:16:50 +0200

    fix: remove unused state fields

--- programs/deposit-program/src/state/user_subscription_info.rs
+++ programs/deposit-program/src/state/user_subscription_info.rs
@@ -3,7 +3,6 @@ use anchor_lang::prelude::*;
 #[account]
 #[derive(InitSpace)]
 pub struct UserSubscriptionInfo {
-    pub available_balance: u64,
     pub expiration: i64,
     pub bump: u8,
 }
--- programs/deposit-program/src/state/user_timed_info.rs
+++ programs/deposit-program/src/state/user_timed_info.rs
@@ -3,6 +3,5 @@ use anchor_lang::prelude::*;
 #[account]
 #[derive(InitSpace)]
 pub struct UserTimedInfo {
-    pub available_balance: u64,
     pub bump: u8,
 }
